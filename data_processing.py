b0fd745749bf5f1079d409cebd724c6a6eb2930d
Kdoger <1377105200@qq.com>
2021-11-01 23:20:09 +0800

    last commit from duolaCmeng

--- data_processing.py
+++ data_processing.py
@@ -10,6 +10,11 @@ from sklearn.model_selection import train_test_split
 
 from statsmodels.formula.api import ols
 from statsmodels.sandbox.regression.predstd import wls_prediction_std
+# 导入模型相关的库
+from sklearn.linear_model import Ridge, RidgeCV, ElasticNet, LassoCV, LassoLarsCV
+from sklearn.model_selection import cross_val_score
+
+
 
 if __name__ == '__main__':
     data = pd.read_csv("data.csv")
@@ -35,8 +40,6 @@ if __name__ == '__main__':
     # 将数据集划分为训练集和测试集
     X_train, X_test, y_train, y_test = train_test_split(feature_data, target_data, test_size=0.3)
 
-
-
     df_train = pd.concat([X_train, y_train], axis=1)
     # ols("target~feature+C(feature)", data=data
     # C(feature)表示这个特征为分类特征category
@@ -53,6 +56,30 @@ if __name__ == '__main__':
     predict_low_upper = pd.DataFrame([lr_model.predict(), iv_l, iv_u], index=['PredictSalePrice', 'iv_l', 'iv_u']).T
     predict_low_upper.plot(kind='hist', alpha=0.4)
     plt.savefig('chart/PredictSalePrices.png')  # 保存图片
+
+    # 岭回归
+    def rmse_cv(model):
+        rmse = np.sqrt(-cross_val_score(model, X_train, y_train, scoring="neg_mean_squared_error", cv=5))
+        return (rmse)
+
+
+    # 导入ridge模型
+    model_ridge = Ridge()
+
+    # 对超参数取值进行猜测和验证
+    alphas = [0.05, 0.1, 0.3, 1, 3, 5, 10, 15, 30, 50, 75]
+    cv_ridge = [rmse_cv(Ridge(alpha=alpha)).mean() for alpha in alphas]
+
+    # 画图查看不同超参数的模型的分数
+    cv_ridge = pd.Series(cv_ridge, index=alphas)
+    cv_ridge.plot(title="Validation - Just Do It")
+    plt.xlabel("alpha")
+    plt.ylabel("rmse")
+    cv_ridge
+
+    # 线性回归
+
+    # 二分类-罗辑回归
 
     plt.show()
 
