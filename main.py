0a5e58427ec8d3a22afdc4af750a7403c50b3305
Kdoger <1377105200@qq.com>
2021-11-03 17:19:03 +0800

    last commit from duolaCmeng

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 import seaborn as sns
+from scipy.stats import skew
 from sklearn.model_selection import train_test_split
 from statsmodels.formula.api import ols
+from scipy.special import boxcox1p
 from statsmodels.sandbox.regression.predstd import wls_prediction_std
 # 导入模型相关的库
 from sklearn.linear_model import Ridge, RidgeCV, ElasticNet, LassoCV, LassoLarsCV
@@ -129,9 +131,9 @@ def linear_regression(X_train, X_test, y_train, y_test, df_train):
     lr_model_YearBuilt = ols("SalePrice~YearBuilt", data=df_train).fit()
 
     # 预测测试集
-    y_hat_GrLivArea = lr_model_GrLivArea(X_test)
-    y_hat_TotalBsmtSF = lr_model_TotalBsmtSF(X_test)
-    y_hat_YearBuilt = lr_model_YearBuilt(X_test)
+    y_hat_GrLivArea = lr_model_GrLivArea.predict(X_test)
+    y_hat_TotalBsmtSF = lr_model_TotalBsmtSF.predict(X_test)
+    y_hat_YearBuilt = lr_model_YearBuilt.predict(X_test)
 
     plt.scatter(X_train['GrLivArea'], y_train, alpha=0.3)
     plt.xlabel('GrLivArea')
@@ -160,21 +162,225 @@ def linear_regression(X_train, X_test, y_train, y_test, df_train):
     print('预测结果：')
     print(lr_model.predict(X_test))
 
-def ridge_regression():
+def ridge_regression(data):
+    feature_data = data
+    target_data = data['SalePrice']
+
+    train, test, y_train, y_test = train_test_split(feature_data, target_data, test_size=0.3)
+
+    print(train.head(5))
+
+    # 1.首先对train进行处理，删除可视化中出现的异常值
+    train = train.drop(train[(train['TotalBsmtSF'] > 5000) & (train['SalePrice'] < 200000)].index)
+    train = train.drop(train[(train['GrLivArea'] > 4000) & (train['SalePrice'] < 200000)].index)
+
+    # 这里采用对数变换的方法使其符合正态分布
+    train["SalePrice"] = np.log(train["SalePrice"])
+
+    # 2.将train和test联合起来一起进行数据处理
+    train_id = train['Id']
+    test_id = test['Id']
+    ntrain = train.shape[0]
+    ntest = test.shape[0]
+    y_train = train.SalePrice.values
+    all_data = pd.concat((train, test)).reset_index(drop=True)
+    all_data.drop(['SalePrice'], axis=1, inplace=True)
+    print("all_data size is : {}".format(all_data.shape))  # all_data size is : (2917, 80)
+
+    # 由于ID对预测没有作用，删除ID字段
+    all_data.drop(['Id'], axis=1, inplace=True)
+
+    # 查看缺失值比率
+    all_data_na = (all_data.isnull().sum() / len(all_data)) * 100
+    all_data_na = all_data_na.drop(all_data_na[all_data_na == 0].index).sort_values(ascending=False)[:30]
+    print(all_data_na)
+    missing_data = pd.DataFrame({'missing_data': all_data_na})
+    print(missing_data.head(20))
+
+    # 对于缺失率在80%以上的特征删除
+    all_data = all_data.drop('PoolQC', axis=1)
+    all_data = all_data.drop('MiscFeature', axis=1)
+    all_data = all_data.drop('Alley', axis=1)
+    all_data = all_data.drop('Fence', axis=1)
+    print(all_data.shape)  # (2917, 75)
+
+    # 对于其他缺失值进行处理, 壁炉为空可能是没有，用none填充
+    all_data['FireplaceQu'] = all_data['FireplaceQu'].fillna('none')
+
+    # LotFrontage代表房屋前街道的长度, 房屋前街道的长度应该和一个街区的房屋相同，可以取同一个街区房屋的街道长度的平均值
+    all_data['LotFrontage'] = all_data.groupby('Neighborhood')['LotFrontage'].transform(lambda x: x.fillna(x.median()))
+
+    # 对于Garage类的4个特征，缺失率一致，一起处理，可能是没有车库，用none填充
+    for c in ('GarageType', 'GarageFinish', 'GarageQual', 'GarageCond'):
+        all_data[c] = all_data[c].fillna('none')
+
+    # 对于garage，同样猜测缺失值缺失的原因可能是因为房屋没有车库，连续型变量用0填充
+    for c in ('GarageYrBlt', 'GarageArea', 'GarageCars'):
+        all_data[c] = all_data[c].fillna(0)
+
+    # 对于地下室相关的连续变量，缺失同样认为房屋可能是没有地下室，用0填充
+    for c in ('BsmtFinSF1', 'BsmtFinSF2', 'BsmtUnfSF', 'TotalBsmtSF', 'BsmtFullBath', 'BsmtHalfBath'):
+        all_data[c] = all_data[c].fillna(0)
+
+    # 地下室相关离散变量，同理用None填充
+    for c in ('BsmtQual', 'BsmtCond', 'BsmtExposure', 'BsmtFinType1', 'BsmtFinType2'):
+        all_data[c] = all_data[c].fillna('None')
+
+    # Mas为砖石结构相关变量，缺失值我们同样认为是没有砖石结构，用0和none填补缺失值
+    all_data["MasVnrType"] = all_data["MasVnrType"].fillna("None")
+    all_data["MasVnrArea"] = all_data["MasVnrArea"].fillna(0)
+
+    # MSZoning代表房屋所处的用地类型，先看下不同取值
+    all_data.groupby('MSZoning')['MasVnrType'].count().reset_index()
+    # 由于业务上房屋类型是必须的，一般都有，考虑用众数填充
+    all_data['MSZoning'] = all_data['MSZoning'].fillna(all_data['MSZoning'].mode()[0])
+
+    # 由于数据Functional缺失即为Typ，所以进行填充Typ
+    all_data["Functional"] = all_data["Functional"].fillna("Typ")
+
+    # 对于Utilities,观察到除了一个“NoSeWa”和2个NA之外，所有记录都是“AllPub”，对于房价预测用处很小，删除这个特征
+    all_data.drop(['Utilities'], axis=1, inplace=True)
+
+    all_data_na = (all_data.isnull().sum() / len(all_data)) * 100
+    all_data_na = all_data_na.drop(all_data_na[all_data_na == 0].index).sort_values(ascending=False)[:30]
+    print(all_data_na)
+    missing_data = pd.DataFrame({'missing_data': all_data_na})
+    print(missing_data)
+
+    # 填充剩余的缺失值
+    for i in missing_data.index:
+        print(all_data[i].head())  # 未展示
+
+    for i in ('SaleType', 'KitchenQual', 'Electrical', 'Exterior2nd', 'Exterior1st'):
+        all_data[i] = all_data[i].fillna(all_data[i].mode()[0])
+
+    # 查看缺失值的比率，发现已经处理完毕，all_data里已经没有缺失值
+    all_data_na = (all_data.isnull().sum() / len(all_data)) * 100
+    all_data_na = all_data_na.drop(all_data_na[all_data_na == 0].index).sort_values(ascending=False)[:30]
+    print(all_data_na)
+    missing_data = pd.DataFrame({'missing_data': all_data_na})
+    print(missing_data)
+
+    # 对于一些数值型特征，数值并不表示大小，将其值转换为字符型
+    all_data['MSSubClass'] = all_data['MSSubClass'].apply(str)
+
+    all_data['OverallCond'] = all_data['OverallCond'].astype(str)
+
+    all_data['YrSold'] = all_data['YrSold'].astype(str)
+    all_data['MoSold'] = all_data['MoSold'].astype(str)
+
+    # 将地下室面积、1楼面积、2楼面积相加得到总面积特征
+    all_data['TotalSF'] = all_data['TotalBsmtSF'] + all_data['1stFlrSF'] + all_data['2ndFlrSF']
+    # 由前面的可视化将房子建造时间做一个划分，以1990进行划分，1990前为0,1990后为1
+    all_data['YearBuilt_cut'] = all_data['YearBuilt'].apply(lambda x: 1 if x > 1990 else 0)
+
+    all_data['Total_sqr_footage'] = (all_data['BsmtFinSF1'] + all_data['BsmtFinSF2'] +
+                                     all_data['1stFlrSF'] + all_data['2ndFlrSF'])
+
+    all_data['Total_Bathrooms'] = (all_data['FullBath'] + (0.5 * all_data['HalfBath']) +
+                                   all_data['BsmtFullBath'] + (0.5 * all_data['BsmtHalfBath']))
+
+    print(all_data.shape)  # (2917, 79)
+
+    # 将all_data分开为训练集与测试集两部分，查看新特征与房价的相关性
+    new_train = all_data[:ntrain]
+    new_test = all_data[ntrain:]
+    new_train['SalePrice'] = y_train
+
+    # 观察到Total_Bathrooms等于5或6时都只有一行，且对应房价较为异常，删除这两个值
+    new_train.loc[:, 'Total_Bathrooms'].value_counts()
+    new_train = new_train.drop(new_train[new_train['Total_Bathrooms'] >= 5.0].index)
+
+    # 将new_train与new_test重新组合成all_data进行数据的统一处理
+    ntrain = new_train.shape[0]
+    ntest = new_test.shape[0]
+    y_train = new_train.SalePrice.values
+    all_data = pd.concat((new_train, new_test)).reset_index(drop=True)
+    all_data.drop(['SalePrice'], axis=1, inplace=True)
+    print("all_data size is : {}".format(all_data.shape))  # all_data size is : (2915, 78)
+
+    # 对有序性离散变量使用label encoder 进行编码
+    from sklearn.preprocessing import LabelEncoder
+
+    cols = ('FireplaceQu', 'BsmtQual', 'BsmtCond', 'GarageQual', 'GarageCond',
+            'ExterQual', 'ExterCond', 'HeatingQC', 'KitchenQual', 'BsmtFinType1',
+            'BsmtFinType2', 'Functional', 'BsmtExposure', 'GarageFinish', 'LandSlope',
+            'LotShape', 'PavedDrive', 'Street', 'CentralAir', 'MSSubClass', 'OverallCond',
+            'YrSold', 'MoSold')
+    for c in cols:
+        lbe = LabelEncoder()
+        lbe.fit(list(all_data[c].values))
+        all_data[c] = lbe.transform(list(all_data[c].values))
+    print(all_data.shape)  # (2915, 78)
+    print(all_data.head(5))
+
+    numeric_feats = all_data.dtypes[all_data.dtypes != "object"].index
+
+    # 查看所有数字特征的偏度
+    skewed_feats = all_data[numeric_feats].apply(lambda x: skew(x.dropna())).sort_values(ascending=False)
+    skewness = pd.DataFrame({'Skew': skewed_feats})
+    print(skewness.head(10))
+
+    # 查看有多少特征的偏度不符合要求，并进行转换
+    skewness = skewness[abs(skewness) > 0.75]
+    print("有{}个特征需要转换 ".format(skewness.shape[0]))
+    # 有59个特征需要转换
+    skewed_features = skewness.index
+    lam = 0.15
+    for feat in skewed_features:
+        # all_data[feat] += 1
+        all_data[feat] = boxcox1p(all_data[feat], lam)
+
+    # 将无序型离散变量转化为哑变量（one-hot编码）
+    all_data = pd.get_dummies(all_data)
+    shreshold = 0.9
+    corr_all_data = all_data.corr().abs()
+    # 取矩阵的上三角部分，判断系数大于0.9的并删除
+    data_up = corr_all_data.where(np.triu(np.ones(corr_all_data.shape), k=1).astype(np.bool))
+
+    drop_col = [column for column in data_up.columns if any(data_up[column] > 0.9)]
+    all_data = all_data.drop(columns=drop_col)
+    print(all_data.shape)  # (2915, 207)
+
+    # 将训练集与测试集分开，用于建模与测试
+    train = all_data[:ntrain]
+    test = all_data[ntrain:]
+    print(train.head())
+
+    def rmse_cv(model):
+        rmse = np.sqrt(-cross_val_score(model, train, y_train, scoring="neg_mean_squared_error", cv=5))
+        return (rmse)
+
+    # 导入ridge模型
+    model_ridge = Ridge()
+
+    # 对超参数取值进行猜测和验证
+    alphas = [0.05, 0.1, 0.3, 1, 3, 5, 10, 15, 30, 50, 75]
+    cv_ridge = [rmse_cv(Ridge(alpha=alpha)).mean() for alpha in alphas]
+
+    # 画图查看不同超参数的模型的分数
+    cv_ridge = pd.Series(cv_ridge, index=alphas)
+    cv_ridge.plot(title="Validation - Just Do It")
+    plt.xlabel("alpha")
+    plt.ylabel("rmse")
+    print(cv_ridge)
+
+    plt.show()
+
     pass
 
 if __name__ == '__main__':
     data = pd.read_csv("data.csv")
 
     # 数据分析
-    data_analysis(data)
+    # data_analysis(data)
 
     # 数据预处理：填补缺省值，删除无用列，划分训练集和测试集
-    X_train, X_test, y_train, y_test, df_train = data_processing(data)
+    # X_train, X_test, y_train, y_test, df_train = data_processing(data)
 
     # 建立回归模型
     # 线性回归模型：一元回归、多元回归
-    linear_regression(X_train, X_test, y_train, y_test, df_train)
+    # linear_regression(X_train, X_test, y_train, y_test, df_train)
 
     # 岭回归模型
-    ridge_regression()
+    ridge_regression(data)
